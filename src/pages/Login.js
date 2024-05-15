6617e13e9ac40de2daa4ad1f37ce24832a04e74b
adilabbasi-786 <m.adilabbasi786@gmail.com>
2024-05-15 14:50:29 +0500

    initial for hotel manager

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -54,10 +54,33 @@ const SignIn = () => {
       }
     } catch (error) {
       console.error("An error occurred:", error);
-      // Handle the error here, for example:
+      alert("please type correct email and password");
       setError("An error occurred. Please try again.");
     }
   };
+  const handleSubmitManager = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post("http://localhost:1337/api/auth/local", {
+        identifier: formData.email,
+        password: formData.password,
+      });
+
+      if (res.data.error) {
+        alert("Invalid email or password");
+      } else {
+        auth.setToken(res.data.jwt);
+        localStorage.setItem("token", res.data.jwt);
+        history.push("/hotel/dashboard");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+      alert("please type correct email and password");
+      setError("An error occurred. Please try again.");
+    }
+  };
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <Container component="main" maxWidth="xs">
@@ -124,16 +147,17 @@ const SignIn = () => {
             >
               Sign In Admin
             </Button>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              id="signInAdmin"
-            >
-              Sign In Hotel Manager
-            </Button>
           </Box>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            id="signInAdmin"
+            onClick={handleSubmitManager}
+          >
+            Sign In Hotel Manager
+          </Button>
         </Box>
       </Container>
     </ThemeProvider>
@@ -141,165 +165,3 @@ const SignIn = () => {
 };
 
 export default SignIn;
-// import * as React from "react";
-// import Avatar from "@mui/material/Avatar";
-// import Button from "@mui/material/Button";
-// import CssBaseline from "@mui/material/CssBaseline";
-// import TextField from "@mui/material/TextField";
-// import FormControlLabel from "@mui/material/FormControlLabel";
-// import Checkbox from "@mui/material/Checkbox";
-// import Link from "@mui/material/Link";
-// import Grid from "@mui/material/Grid";
-// import Box from "@mui/material/Box";
-// import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-// import Typography from "@mui/material/Typography";
-// import Container from "@mui/material/Container";
-// import { createTheme, ThemeProvider } from "@mui/material/styles";
-// import axios from "axios";
-// import { useState } from "react";
-// function Copyright(props) {
-//   return (
-//     <Typography
-//       variant="body2"
-//       color="text.secondary"
-//       align="center"
-//       {...props}
-//     >
-//       {"Copyright © "}
-//       <Link color="inherit" href="/admin">
-//         Your Website
-//       </Link>{" "}
-//       {new Date().getFullYear()}
-//       {"."}
-//     </Typography>
-//   );
-// }
-
-// const defaultTheme = createTheme();
-
-// export default function SignIn() {
-//   const [formData, setFormData] = useState({
-//     email: "",
-//     password: "",
-//   });
-//   const [error, setError] = useState(null);
-//   const handleInputChange = (e) => {
-//     const { name, value } = e.target;
-//     setFormData((prevData) => ({
-//       ...prevData,
-//       [name]: value,
-//     }));
-//   };
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const res = await axios.post("http://localhost:1337/auth/local", {
-//         identifier: formData.email,
-//         password: formData.password,
-//       });
-//       const { jwt } = res.data;
-//       localStorage.setItem("jwt", jwt);
-//       window.location.href = "/dashboard";
-//     } catch (error) {
-//       setError("Incorrect email or password");
-//     }
-//   };
-
-//   return (
-//     <ThemeProvider theme={defaultTheme}>
-//       <Container component="main" maxWidth="xs">
-//         <CssBaseline />
-//         <Box
-//           sx={{
-//             marginTop: 8,
-//             display: "flex",
-//             flexDirection: "column",
-//             alignItems: "center",
-//           }}
-//         >
-//           <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-//             <LockOutlinedIcon />
-//           </Avatar>
-//           <Typography component="h1" variant="h5">
-//             Sign in
-//           </Typography>
-//           <Box
-//             component="form"
-//             onSubmit={handleSubmit}
-//             noValidate
-//             sx={{ mt: 1 }}
-//           >
-//             <TextField
-//               margin="normal"
-//               required
-//               fullWidth
-//               id="email"
-//               label="Email Address"
-//               name="email"
-//               autoComplete="email"
-//               autoFocus
-//               value={formData.email}
-//               onChange={handleInputChange}
-//             />
-//             <TextField
-//               margin="normal"
-//               required
-//               fullWidth
-//               name="password"
-//               label="Password"
-//               type="password"
-//               id="password"
-//               autoComplete="current-password"
-//               value={formData.password}
-//               onChange={handleInputChange}
-//             />
-//             {error && (
-//               <Typography color="error" variant="body2">
-//                 {error}
-//               </Typography>
-//             )}
-//             <FormControlLabel
-//               control={<Checkbox value="remember" color="primary" />}
-//               label="Remember me"
-//             />
-//             <Link color="inherit" href="/admin/dashboard">
-//               <Button
-//                 //   type="submit"
-//                 fullWidth
-//                 variant="contained"
-//                 sx={{ mt: 3, mb: 2 }}
-//                 id="signInAdmin"
-//               >
-//                 Sign In Admin
-//               </Button>
-//             </Link>
-//             <Link color="inherit" href="/hotel/dashboard">
-//               <Button
-//                 //   type="submit"
-//                 fullWidth
-//                 variant="contained"
-//                 sx={{ mt: 3, mb: 2 }}
-//               >
-//                 Sign In hotel
-//               </Button>
-//             </Link>
-
-//             <Grid container>
-//               <Grid item xs>
-//                 <Link href="#" variant="body2">
-//                   Forgot password?
-//                 </Link>
-//               </Grid>
-//               <Grid item>
-//                 <Link href="#" variant="body2">
-//                   {"Don't have an account? Sign Up"}
-//                 </Link>
-//               </Grid>
-//             </Grid>
-//           </Box>
-//         </Box>
-//         <Copyright sx={{ mt: 8, mb: 4 }} />
-//       </Container>
-//     </ThemeProvider>
-//   );
-// }
